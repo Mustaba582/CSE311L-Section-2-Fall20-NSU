c620db88fda52f03d2b045069566b8a45bda9497
1911076642-Mustaba Mubasweer <mustaba.mubasweer@northsouth.edu>
2021-01-02 09:46:51 +0600

    file added

--- Quiz and Lab Assessment/2.php
+++ Quiz and Lab Assessment/2.php
@@ -12,7 +12,7 @@ if ($connection == false) {
 }
 
 
-$sql = "SELECT manager_id, min(salary) from employee where manager_id is not null group by manager_id having min(salary) > 6000 order by min(salary) desc;";
+$sql = "SELECT manager_id, min(salary) from employees where manager_id is not null group by manager_id having min(salary) > 6000 order by min(salary) desc;";
 $result = mysqli_query($connection, $sql);
 
 
